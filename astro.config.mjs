d65b66664ff3f29cd8bad1d185f23f0b070d590e
jadoonoona <gkawlgml512@naver.com>
2026-05-14 15:07:56 +1000

    Update site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://whydevils.github.io',
-  base: '/astro-scholar/',
+  site: 'https://jadoonoona.github.io',
+  base: '/mathblog/',
 });

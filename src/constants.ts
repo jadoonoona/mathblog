62e5701ac360057f0bb5cd6dc514a31e8b7891b1
jadoonoona <gkawlgml512@naver.com>
2026-05-15 22:12:16 +1000

    restructuring

--- src/constants.ts
+++ src/constants.ts
@@ -18,7 +18,7 @@ export const SITE_DATA = {
 
     // Bluesky handle (without the @)
     bluesky: "",
-    linkedin: "[your-linkedin-template]",
+    linkedin: "jihee-h",
     github: "jadoonoona",
 
     // Google Scholar ID (found in your Scholar profile URL)
